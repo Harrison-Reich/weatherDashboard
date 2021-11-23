7a44ad8e3204356e5ecb750d46945007080c7b14
Harrison Reich <hreich1998@gmail.com>
2021-11-22 22:22:06 -0800

    updated script

--- script.js
+++ script.js
@@ -29,6 +29,31 @@ let weather = {
   },
 };
 
+let weekForcast = {
+  apiKey: "86296f2ec0b9ce69883449dd897f8071",
+  getWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid="this.apiKey"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => this.showWeather(data));
+  },
+  showWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.getElementById("city").innerText = "Weather in " + name;
+    document.getElementById("icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.getElementById("description").innerText = description;
+    document.getElementById("temp").innerText = temp + "°F";
+    document.getElementById("humidity").innerText = "Humidity: " + humidity + "%";
+    document.getElementById("wind").innerText = "Wind speed: " + speed + " mph";
+  },
+};
+
 document.getElementById("searchButton").addEventListener("click", function () {
   weather.search();
 });
